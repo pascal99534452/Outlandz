15495297c5db7bf92a69826dccde18c48e0e156f
pascal99534452 <farikored.info@gmail.com>
2020-03-14 21:12:13 +0100

    g
    g

--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
     // Id van category van tickets.
-    const categoryId = "687253002519707689";
+    const categoryId = "686844611968237568";
 
     // Als bericht in ticket kanaal is dan verwijder kanaal ander zend bericht
     if (message.channel.parentID == categoryId) {
